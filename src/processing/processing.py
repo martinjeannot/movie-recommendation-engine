a376aea7aeac68369f5fda36c520ba702908e7ed
Martin Jeannot <m4rtinjeannot@gmail.com>
2020-11-14 21:19:47 +0100

    [Processing] Remove movies not rated by anybody from our list

--- src/processing/processing.py
+++ src/processing/processing.py
@@ -8,6 +8,12 @@ def main():
     data_path = (Path(__file__).parent / '../../data').resolve()
     ratings = pd.read_csv(data_path / 'raw/ml-latest-small/ratings.csv')
     ratings_matrix = ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0)
+
+    # remove movies not rated by anybody (ensuring we can correctly match movie ids and indices)
+    movies = pd.read_csv(data_path / 'raw/ml-latest-small/movies.csv')
+    movies = movies[movies.movieId.isin(ratings_matrix.columns)]
+    movies.to_csv(data_path / 'processed/movies.csv', index=False)
+
     ratings_matrix = ratings_matrix.to_numpy()
     with open(data_path / 'processed/ratings.npy', 'wb') as f:
         np.save(f, ratings_matrix)
